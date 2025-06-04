daa915b7e1352e28ba43f52d323f0985cd14255b
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-06-04 20:24:44 +0530

    ..

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,4 +1,4 @@
-"use clint"
+"use client"
 import axios from "axios"
 import Link from "next/link"
 import React, { useEffect, useState } from "react"
@@ -8,25 +8,35 @@ export default function verifyEmailPage() {
     const [verified, setverified] = useState(false)
     const [error, seterror] = useState<Boolean>(false)
 
-    const verifyUserEmail = async()=>{
+    const verifyUserEmail = async () => {
         try {
-            await axios.post('/api/users/verifyemail',{token})
+            await axios.post('/api/users/verifyemail', { token })
             setverified(true)
-        } catch (error:any) {
+        } catch (error: any) {
             seterror(true)
             console.log(error.response.data);
         }
     }
 
     useEffect(() => {
-      const urlToken = window.location.search.split("=")[1]
-      settoken(urlToken)
+        const urlToken = window.location.search.split("=")[1]
+        settoken(urlToken || "")
     }, [])
 
     useEffect(() => {
-      if (token.length > 0) {
-        verifyUserEmail()
-      }
+        if (token?.length > 0) {
+            verifyUserEmail()
+        }
     }, [token])
-    
+
+    return (
+        <>
+            <h1>verify email</h1>
+            <h2>{token ? `${token}` : "no token"}</h2>
+            <h2>{verified && "email verified"}</h2>
+            <Link href='/login'>Login</Link>
+            <h2>{error && "Error"}</h2>
+        </>
+    )
+
 }
