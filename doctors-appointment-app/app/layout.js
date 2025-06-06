11359d938c3d70f786b418c193bb0412863543c0
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-06-06 20:48:37 +0530

    shadcn

--- doctors-appointment-app/app/layout.js
+++ doctors-appointment-app/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,16 +11,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body className={`${inter.className}`}>
-        {/* header */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* header */}
 
-        <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}</main>
 
-        {/* footer */}
-        <footer>
-          
-        </footer>
+          {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>hehe</p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
--- doctors-appointment-app/app/page.js
+++ doctors-appointment-app/app/page.js
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>dr app</div>
+    <div>
+      dr app<br/>
+      <Button>Click</Button>
+    </div>
   );
 }
