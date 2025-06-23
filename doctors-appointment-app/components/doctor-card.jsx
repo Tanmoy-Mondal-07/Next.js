3984f09f8a27a4091cf900a218b255a9bcbcd3b6
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-06-23 19:53:53 +0530

    ..

--- doctors-appointment-app/components/doctor-card.jsx
+++ doctors-appointment-app/components/doctor-card.jsx
@@ -2,27 +2,31 @@ import React from 'react'
 import { Card, CardContent } from './ui/card'
 import { User } from 'lucide-react'
 
-function DoctorCard({doctor}) {
-  return (
-    <Card className="border-emerald-900/20 hover:border-emerald-600 transition-all">
-        <CardContent>
-            <div>
-                <div className='w-12 h-12 rounded-full bg-emerald-900/20 flex items-center justify-center flex-shrink-0'>
-                    {doctor.imageUrl?(
-                        <img src={doctor.imageUrl} className='w-12 h-12 rounded-full object-cover'/>
-                    ):(
-                        <User className='h-6 w-6 text-emerald-400'/>
-                    )}
-                </div>
+function DoctorCard({ doctor }) {
+    return (
+        <Card className="border-emerald-900/20 hover:border-emerald-600 transition-all">
+            <CardContent>
+                <div>
+                    <div className='w-12 h-12 rounded-full bg-emerald-900/20 flex items-center justify-center flex-shrink-0'>
+                        {doctor.imageUrl ? (
+                            <img src={doctor.imageUrl} className='w-12 h-12 rounded-full object-cover' />
+                        ) : (
+                            <User className='h-6 w-6 text-emerald-400' />
+                        )}
+                    </div>
 
-                <div className=''>
-                    {doctor.description}
-                </div>
-            </div>
-        </CardContent>
+                    <p className='text-sm text-muted-foreground mb-1'>
+                        {doctor.speciality}.{doctor.exprience} years exprience
+                    </p>
 
-    </Card>
-  )
+                    <div className='mt-4 line-clamp-2 text-sm text-muted-foreground mb-4'>
+                        {doctor.description}
+                    </div>
+                </div>
+            </CardContent>
+
+        </Card>
+    )
 }
 
 export default DoctorCard
