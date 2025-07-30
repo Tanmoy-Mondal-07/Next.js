6efcf107cef57c51902e669b80cddee8816692cf
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-30 19:30:54 +0530

    ..

--- message/src/app/model/User.ts
+++ message/src/app/model/User.ts
@@ -1,8 +1,51 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-export interface Message extends Document{
-    content: String;
+export interface Message extends Document {
+    content: string;
     createdAt: Date;
 }
 
-const MessageSchema = 
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMesage: boolean;
+    message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/,"please use valid email address"]  //rajax
+    },
+    password:{
+        type: String,
+        required:[true,"Password is required"]
+    },
+    verifyCode:{
+        type: String,
+        required:[true,"Password is required"]
+    }
+})
