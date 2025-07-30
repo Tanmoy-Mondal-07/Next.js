6e35ba13ea7cb2c5be3c970d0300db72a852bbb9
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-30 20:46:19 +0530

    ..

--- message/src/model/User.ts
+++ message/src/model/User.ts
@@ -23,6 +23,7 @@ export interface User extends Document {
     password: string;
     verifyCode: string;
     verifyCodeExpiry: Date;
+    isVerified: boolean;
     isAcceptingMesage: boolean;
     message: Message[];
 }
@@ -38,14 +39,31 @@ const UserSchema: Schema<User> = new Schema({
         type: String,
         required: [true, "Email is required"],
         unique: true,
-        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/,"please use valid email address"]  //rajax
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "please use valid email address"]  //rajax
     },
-    password:{
+    password: {
         type: String,
-        required:[true,"Password is required"]
+        required: [true, "Password is required"]
     },
-    verifyCode:{
+    verifyCode: {
         type: String,
-        required:[true,"Password is required"]
-    }
+        required: [true, "Verify Code is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify Code Expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMesage: {
+        type: Boolean,
+        default: true
+    },
+    message: [MessageSchema]
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel
