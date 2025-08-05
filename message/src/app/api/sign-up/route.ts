cc58ee8b833c585c4436740eb985e35d18ec6fa2
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-08-05 20:36:02 +0530

    ..

---
+++ message/src/app/api/sign-up/route.ts
@@ -0,0 +1,22 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+    await dbConnect()
+    try {
+        const { username, email, password } = await request.json()
+        
+    } catch (error) {
+        console.log('error regestring user :: ', error);
+        return Response.json(
+            {
+                success: false,
+                message: 'Error registring user'
+            }, {
+            status: 500
+        }
+        )
+    }
+}
