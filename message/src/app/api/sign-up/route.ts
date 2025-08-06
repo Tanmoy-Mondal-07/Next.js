fbb55a5b536a2a01e3b08b5ef2ecd17eac64963a
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-08-06 19:54:16 +0530

    ..

--- message/src/app/api/sign-up/route.ts
+++ message/src/app/api/sign-up/route.ts
@@ -7,7 +7,28 @@ export async function POST(request: Request) {
     await dbConnect()
     try {
         const { username, email, password } = await request.json()
-        
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success: false,
+                message: "username is alredy taken"
+            }, { status: 400 })
+        }
+
+        const existingUserByEmail = await UserModel.findOne({
+            email
+        })
+
+        if (existingUserByEmail) {
+            true//todo
+        } else {
+            const hasedPassword = await bcrypt.hash(password, 10)
+        }
+
     } catch (error) {
         console.log('error regestring user :: ', error);
         return Response.json(
