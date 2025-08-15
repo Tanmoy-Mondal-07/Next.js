5db94fb20eede51193d543ede93e48c2518b28cd
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-08-15 20:22:28 +0530

    ..

--- message/src/app/api/check-username-unique/route.ts
+++ message/src/app/api/check-username-unique/route.ts
@@ -2,3 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { z } from "zod"
 import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
