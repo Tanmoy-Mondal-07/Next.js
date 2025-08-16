86b7fdbc20cd7e49e60c50760780c3d58a6a1068
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-08-16 22:09:09 +0530

    ..

--- message/src/app/api/check-username-unique/route.ts
+++ message/src/app/api/check-username-unique/route.ts
@@ -18,6 +18,23 @@ export async function GET(request: Request) {
 
         //validet with zod
         const result = UsernameQuerySchema.safeParse(queryParam)
+        // console.log(result);
+
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || []
+            return Response.json({
+                success: false,
+                message: usernameErrors?.length > 0 ? usernameErrors.join(',') : 'invalid query parameters'
+            }, { status: 400 })
+        }
+
+        const { username } = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true })
+
+        if (existingVerifiedUser) {
+            
+        }
 
     } catch (error) {
         console.log("Error while checking username :: ", error);
