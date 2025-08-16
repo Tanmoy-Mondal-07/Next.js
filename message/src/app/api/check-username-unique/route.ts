6a8313529456988173d69faa1e359d6306dbd67f
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-08-16 19:28:33 +0530

    ..

--- message/src/app/api/check-username-unique/route.ts
+++ message/src/app/api/check-username-unique/route.ts
@@ -1,8 +1,29 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { z } from "zod"
+import { success, z } from "zod"
 import { usernameValidation } from "@/schemas/signUpSchema";
 
 const UsernameQuerySchema = z.object({
     username: usernameValidation
 })
+
+export async function GET(request: Request) {
+    await dbConnect()
+
+    try {
+        const { searchParams } = new URL(request.url)
+        const queryParam = {
+            username: searchParams.get('username')
+        }
+
+        //validet with zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+
+    } catch (error) {
+        console.log("Error while checking username :: ", error);
+        return Response.json({
+            success: false,
+            message: "Error checking username"
+        }, { status: 500 })
+    }
+}
