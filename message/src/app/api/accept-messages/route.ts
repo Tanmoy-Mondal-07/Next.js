3c67790f65625d4ceebda7b92f83eb2b71cdb43a
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-08-20 20:51:16 +0530

    ..

--- message/src/app/api/accept-messages/route.ts
+++ message/src/app/api/accept-messages/route.ts
@@ -51,5 +51,40 @@ export async function POST(request: Request) {
 }
 
 export async function GET(request: Request) {
+    await dbConnect()
 
+    const session = await getServerSession(authOptions)
+    const user: User = session?.user as User
+
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not Authenticated"
+        }, { status: 401 })
+    }
+
+    const userId = user._id
+
+    try {
+        const foundUser = await UserModel.findById(userId)
+
+        if (!foundUser) {
+            return Response.json({
+                success: false,
+                message: "user not found"
+            }, { status: 404 })
+        }
+
+        return Response.json({
+            success: true,
+            isAcceptingMessages: foundUser.isAcceptingMesage
+        }, { status: 200 })
+
+    } catch (error) {
+        console.log("failed to get accpet messages GET");
+        return Response.json({
+            success: false,
+            message: "failed to get accpet messages status"
+        }, { status: 500 })
+    }
 }
