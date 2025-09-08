e2ce66608e4ac800f88e4c9c09f1f67334f0d8cd
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-09-08 19:56:15 +0530

    ..

--- message/src/components/Navbar.tsx
+++ message/src/components/Navbar.tsx
@@ -4,13 +4,26 @@ import React from 'react'
 import Link from 'next/link'
 import { useSession, signOut } from 'next-auth/react'
 import { User } from 'next-auth'
+import { Button } from './ui/button'
 
 function Navbar() {
 
     const { data: session } = useSession()
-    
+    const user: User = session?.user as User
     return (
-        <div>Navbar</div>
+        <nav>
+            <div>
+                <a href='#'>Mystry Message</a>
+                {
+                    session ? (
+                        <>
+                            <span>Welcome, {user.username || user.email}</span>
+                            <Button onClick={() => signOut()}>Logout</Button>
+                        </>
+                    ) : ()
+                }
+            </div>
+        </nav>
     )
 }
 
