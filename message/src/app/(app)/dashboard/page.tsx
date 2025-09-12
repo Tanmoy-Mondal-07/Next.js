7e8e2b418af8f83906160b79e19b344945ca5bdd
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-09-12 20:30:33 +0530

    ..

--- message/src/app/(app)/dashboard/page.tsx
+++ message/src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+'use client'
+import { Message } from '@/model/User'
+import React, { useState } from 'react'
 
 function page() {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
   return (
     <div>page</div>
   )
