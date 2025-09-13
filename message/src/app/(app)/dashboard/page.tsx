29414d3108a23cfd7a22d75ab19678bd1c80f345
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-09-13 20:35:28 +0530

    ..

--- message/src/app/(app)/dashboard/page.tsx
+++ message/src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { Message } from '@/model/User'
 import { AcceptMessageSchema } from '@/schemas/acceptMessageSchema'
+import { ApiResponse } from '@/types/ApiResponse'
 import { zodResolver } from '@hookform/resolvers/zod'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { useSession } from 'next-auth/react'
 import React, { useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
 function page() {
   const [messages, setMessages] = useState<Message[]>([])
@@ -32,9 +34,25 @@ function page() {
       const responce = await axios.get('/api/accept-messages')
       setValue('acceptMessages', responce.data.isAcceptingMesage)
     } catch (error) {
-
+      const axiosError = error as AxiosError<ApiResponse>
+      toast.error("Error", {
+        description: axiosError.response?.data.message || "Faild to fetch message settings"
+      })
+    } finally {
+      setIsLoading(false)
     }
   }, [setValue])
+
+  const fetchMessages = useCallback(async (refresh: boolean = false) => {
+    setIsLoading(true)
+    setIsSwitchLoading(false)
+    try {
+      const responce = await axios.get<ApiResponse>('/api/get-messages')
+      setMessages(responce.data.messages || [])
+    } catch (error) {
+      
+    }
+  }, [])
 
   return (
     <div>page</div>
