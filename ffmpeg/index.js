b9e0a58b9f376c340a7e4c74361ef6b68b788d41
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-09-20 18:55:37 +0530

    ..

--- ffmpeg/index.js
+++ ffmpeg/index.js
@@ -3,6 +3,10 @@ import cors from 'cors'
 import multer from 'multer'
 import { v4 as uuidv4 } from 'uuid'
 import path from "path"
+import fs from "fs"
+import { exec } from "child_process"
+import { error } from 'console'
+import { stderr, stdout } from 'process'
 
 const app = express()
 
@@ -13,9 +17,12 @@ const storage = multer.diskStorage({
         cb(null, "./uploads")
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + "_" + uuidv4() + path.extname)
+        cb(null, file.fieldname + "_" + uuidv4() + path.extname(file.originalname))
     }
 })
+
+//multer config
+const upload = multer({ storage: storage })
 
 app.use(
     cors({
@@ -35,6 +42,23 @@ app.use("/uploads", express.static("uploads"))
 
 app.get('/', function (req, res) {
     res.json({ message: "working" })
+})
+
+app.post("/upload", upload.single('file'), function (req, res) {
+    const lessonId = uuidv4()
+    const videoPath = req.file.path
+    const outputPath = `./uploads/courses/${lessonId}`
+    const hlsPath = `${outputPath}/index.m3u8`
+    console.log("hlspath", hlsPath)
+
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, { recursive: true })
+    }
+
+    //ffmpeg
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`
+
+    exec(ffmpegCommand, (error, stdout, stderr))
 })
 
 app.listen(8000, function () {
