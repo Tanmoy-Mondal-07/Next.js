8033756ef03efc0fa949b213735ef08bac7bfaf2
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-09-21 20:38:21 +0530

    ..

--- 3d/spline01/src/App.jsx
+++ 3d/spline01/src/App.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
-    <div style={{ width: "60vw", height: "100vh" }}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Spline scene="https://prod.spline.design/6sODxXr7vPN6Jgqf/scene.splinecode" />
     </div>
   )
