4ea8f38c552c31d9b69e70396ad74f2640113e46
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-09-25 20:09:21 +0530

    ..

--- project-p/src/app/page.tsx
+++ project-p/src/app/page.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      
+
       <ParticleCanvas />
+      
       <Spline scene="https://prod.spline.design/e9FijfUkjE6I9kqF/scene.splinecode" className="absolute inset-0 z-50"/>
     </div>
   );
